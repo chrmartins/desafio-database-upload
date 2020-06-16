fa9fff5d038ffea142db4aa85a80b909f4266fed
chrmartins <cmts2010@gmail.com>
2020-06-16 18:19:21 -0300

    migrations run

---
+++ src/database/migrations/1592335644849-CreateCategories.ts
@@ -0,0 +1,41 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateCategories1592335644849
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Criando a Tabela
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'create_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'update_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Para desfazer a tabela criada
+    await queryRunner.dropTable('categories');
+  }
+}
